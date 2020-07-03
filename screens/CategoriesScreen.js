161fdd1e11a57d3eccadc5d69243703e8642bcb5
Hannah <hannahjoo24@gmail.com>
2020-07-02 17:11:20 -0700

    Outputting a grid of categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,30 +1,53 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, FlatList } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data'; // array
+
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoriesScreen = props => {
   // console.log('props ', props);
 
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button title="Go to Meals!" onPress={() => {
-        props.navigation.navigate({routeName: 'CategoryMeals'}); // routeName should be the same as one of keys from MealsNavigator.js
-
-        // same as above
-        // props.navigation.push('CategoryMeals');
-
-        // 'replace' => stack will be empty - e.g. (log in screen) After logging in, a user should not be able to go back to the previous log in page
-        // props.navigation.replace('CategoryMeals'); 
-      }} />
-    </View>
+    <FlatList 
+      // keyExtractor={(item, index) => item.id} // older version needs this!
+      data={CATEGORIES} 
+      renderItem={renderGridItem} 
+      numColumns={2} // how many columns I want
+    /> 
   )
 };
+
+{/* <View style={styles.screen}>
+  <Text>The Categories Screen!</Text>
+  <Button title="Go to Meals!" onPress={() => {
+    props.navigation.navigate({routeName: 'CategoryMeals'}); // routeName should be the same as one of keys from MealsNavigator.js
+
+    // same as above
+    // props.navigation.push('CategoryMeals');
+
+    // 'replace' => stack will be empty - e.g. (log in screen) After logging in, a user should not be able to go back to the previous log in page
+    // props.navigation.replace('CategoryMeals'); 
+  }} />
+</View>  */}
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
 
